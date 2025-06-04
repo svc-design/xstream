1a8201c8bb9d11e0f4a22e4d73639bbaaa2b149b
shenlan <manbuzhe2009@qq.com>
2025-06-04 20:59:26 +0800

    chore: remove iOS method channel stubs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,9 +8,6 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    if let controller = window?.rootViewController as? FlutterViewController {
-      setupMethodChannel(controller)
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
