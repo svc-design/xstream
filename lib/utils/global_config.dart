bd969def18c9718aa78a0238b471d9e8c487d7b3
shenlan <manbuzhe2009@qq.com>
2025-06-14 19:19:29 +0800

    Update global_config.dart

--- lib/utils/global_config.dart
+++ lib/utils/global_config.dart
@@ -101,7 +101,7 @@ class GlobalApplicationConfig {
         return '$home/Library/LaunchAgents/$serviceName';
       case 'linux':
         final home = Platform.environment['HOME'] ?? '';
-        return '$home/.config/systemd/user/$serviceName';
+        return '/etc/systemd/system/$serviceName';
       case 'windows':
         final base = Platform.environment['ProgramData'] ?? 'C:\\ProgramData';
         return '$base\\xstream\\$serviceName';
